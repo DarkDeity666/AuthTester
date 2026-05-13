4ba68103d9203c9138d3f4ca987534872b6327ed
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-05-14 02:04:02 +0530

    Added route imports

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-import express from "express";
+import express, { json } from "express";
 import morgan from "morgan";
-// import dotenv from "dotenv"
+import authRouter from "./routes/auth.route.js"
 
 
 const app = express();
-// dotenv.config();
+//middelware
 app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use("api/auth",authRouter);
 
 
-
-
-
-export default app; 
+export default app;
