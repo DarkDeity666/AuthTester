610b3b7f5acbcbb2c3e70c01e4a95d20cddf01ac
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-05-23 00:37:12 +0530

    Feature- Enhanced tokanization for better access(refreshToken, access token and newRefreshToken)

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,12 @@
-import {Router} from "express";
-import { registerUser,userLogin } from "../controllers/auth.controller.js"
+import { Router } from "express";
+import { registerUser, userLogin, getMe,refreshToken } from "../controllers/auth.controller.js"
 const authRouter = Router();
 
 
-authRouter.post("/register",registerUser)
-authRouter.post("/login",userLogin)
-
+authRouter.post("/register", registerUser)
+authRouter.post("/login", userLogin)
+authRouter.get("/get-me",getMe) 
+authRouter.get("/refresh-token",refreshToken)
 
 
 
