703e5bf040784360e8367702115c33bbbef82e7d
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2026-05-24 10:03:25 +0530

    Feature - routes and controllers for userLogin,userRegister,refresh and access token,userLogout and logout from all devices

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, userLogin, getMe,refreshToken } from "../controllers/auth.controller.js"
+import { registerUser, userLogin, getMe,refreshToken,userLogout,userlogoutAll } from "../controllers/auth.controller.js"
 const authRouter = Router();
 
 
@@ -7,7 +7,7 @@ authRouter.post("/register", registerUser)
 authRouter.post("/login", userLogin)
 authRouter.get("/get-me",getMe) 
 authRouter.get("/refresh-token",refreshToken)
-
-
+authRouter.post("/logout",userLogout)
+authRouter.post("/logout-all",userlogoutAll)
 
 export default authRouter;
